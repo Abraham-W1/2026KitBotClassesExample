334c960c0f4b344d0ea1f85897c44781bef68f6e
Abraham <wondie@unlv.nevada.edu>
2026-01-17 10:55:23 -0800

    Update 1

--- src/main/java/frc/robot/commands/AutoAlign.java
+++ src/main/java/frc/robot/commands/AutoAlign.java
@@ -1,8 +1,10 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.CANDriveSubsystem;
 
-public class AutoAlign {
+
+public class AutoAlign extends Command{
     private final CANDriveSubsystem driveSubsystem;
     private final String limelightName;
     private final double kP = 0.035;
@@ -10,11 +12,20 @@ public class AutoAlign {
     public AutoAlign(CANDriveSubsystem driveSubsystem, String limelightName){
         this.driveSubsystem = driveSubsystem;
         this.limelightName = limelightName;
-        //addRequirements(driveSubsystem);
+        addRequirements(driveSubsystem);
     }
 
     public void execute(){
-        
+        //double rotSpeed = TagAlignment.aimAtTag(limelightName, kP);
+        //driveSubsystem.drive(0, 0, rotSpeed, true);
     }
 
+    public boolean isFinished(){
+        //return TagAlignment.isAlignedWithTag(limelightName, 2.0);
+        return false; 
+    }
+
+    public void end(boolean interrupted){
+        //drive.drive(0, 0, 0, true);
+    }
 }
